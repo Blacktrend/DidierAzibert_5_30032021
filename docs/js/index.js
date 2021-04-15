197ecfedeb12823201df440bcf600ec6e94e0d0a
Didier Azibert <dazibert@adipao.com>
2021-04-15 13:29:00 +0400

    wip - js

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,31 +1,43 @@
-/* création d'une requête Ajax pour
- * récupérer la liste des ours en peluche
-*/
-let request = new XMLHttpRequest();
-request.open("GET", "http://localhost:3000/api/teddies/"); // méthode GET depuis l'url de l'api correspondante
-request.send();
-request.onreadystatechange = function() { // fonction appelée à chaque changement d'état de la requête
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) { // test si la requête est terminée et si le code http renvoyé est OK
-        let results = JSON.parse(this.responseText); // réponse de l'API convertie en objet JS
-        let container = document.getElementById("cards"); // on cible le div qui contiendra les vignettes
-
-        for (let result of results) { // boucle pour afficher chaque vignette selon le nombre de produits
-            let id = result._id; // récupération des données utiles pour chaque produit
-            let imgUrl = result.imageUrl;
-            let name = result.name;
-            let price = (result.price / 100).toFixed(2) + "€"; // mise en forme du prix
-            let description = result.description;
-            let card = document.createElement("div"); // création du div vignette
-            card.classList.add("col-11", "col-sm-5", "col-lg-3", "card", "m-2", "py-3"); // ajout des classes Bootstrap
-
-            // ajout du contenu de la vignette en utilisant la syntaxe des template literals
-            card.innerHTML = `<img src="${imgUrl}" class="card-img-top" alt="${description}"/>
-<div class="card-body d-flex flex-column justify-content-end">
-    <h2 class="card-title">${name}</h2>
-    <p class="card-text"><span class="font-weight-bold">${price}</span></p>
-    <a href="product.html?id=${id}" class="btn btn-primary">VOIR</a>
-</div>`;
-            container.appendChild(card); // ajout de la vignette au div conteneur
+/**
+ * requête Ajax pour récupérer la liste des produits
+ */
+function getProducts() {
+    const request = new XMLHttpRequest();
+    request.open("GET", "http://localhost:3000/api/teddies/"); // méthode GET depuis l'url de l'api correspondante
+    request.send();
+    request.onreadystatechange = function () { // fonction appelée à chaque changement d'état de la requête
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) { // test si la requête est terminée et si le code http renvoyé est OK
+            const results = JSON.parse(this.responseText); // réponse de l'API convertie en objet JS
+            displayProducts(results);
         }
+    };
+}
+
+function displayProducts(products) {
+    const container = document.getElementById("cards"); // on cible le div qui contiendra les vignettes
+
+    for (let product of products) { // boucle pour afficher chaque vignette selon le nombre de produits
+        /**
+         * product
+         * @type {{_id: string, imageURL: string, name: string, price: number, description: string}}
+         */
+        const id = product._id; // récupération des données utiles pour chaque produit
+        const imgUrl = product.imageUrl;
+        const name = product.name;
+        const price = (product.price / 100).toFixed(2) + "€"; // mise en forme du prix
+        const description = product.description;
+        const card = document.createElement("div"); // création du div vignette
+        card.classList.add("col-11", "col-sm-5", "col-lg-3", "card", "m-2", "py-3"); // ajout des classes Bootstrap
+
+        // ajout du contenu de la vignette en utilisant la syntaxe des template literals
+        card.innerHTML = `<img src="${imgUrl}" class="card-img-top" alt="${description}"/>
+    <div class="card-body d-flex flex-column justify-content-end">
+        <h2 class="card-title">${name}</h2>
+        <p class="card-text"><span class="font-weight-bold">${price}</span></p>
+        <a href="product.html?id=${id}" class="btn btn-primary">VOIR</a>
+    </div>`;
+        container.appendChild(card); // ajout de la vignette au div conteneur
     }
-};
+}
+
+getProducts();
--- docs/js/product.js
+++ docs/js/product.js
@@ -1,38 +1,46 @@
-// récupération du paramètre id de l'url
-async function getProductId() {
+/**
+ * récupération du paramètre id de l'url
+ * @returns {string}
+ */
+function getProductId() {
     const urlParams = new URLSearchParams(location.search); // récupération des paramètres de l'url
     return urlParams.get("id"); // renvoie l'id du produit indiqué dans l'url
 }
 
-
-
-
-
-
-
-// requête Ajax pour récupérer les données du produit
-async function getProductObject() {
-    const id = await getProductId();
-    let request = new XMLHttpRequest();
+/**
+ * requête Ajax pour récupérer les données du produit
+ */
+function getProductObject() {
+    const id = getProductId();
+    const request = new XMLHttpRequest();
     request.open("GET", "http://localhost:3000/api/teddies/" + id);
     request.send();
     request.onreadystatechange = function() {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            let product = JSON.parse(this.responseText);
+            /**
+             * product
+             * @type {{name: string, price: number, colors: string, description: string, imageUrl: string}}
+             */
+            const product = JSON.parse(this.responseText);
+            document.getElementById("breadcrumb").innerText = product.name;
+            document.getElementById("name").innerText = product.name;
+            document.getElementById("description").innerText = product.description;
+            document.getElementById("price").innerText = (product.price / 100).toFixed(2) + "€";
+            document.getElementById("img").setAttribute("src", product.imageUrl);
+            document.getElementById("img").setAttribute("alt", product.name + "- Ours en peluche fait main");
+            document.getElementById("img").setAttribute("title", product.name + "- Ours en peluche fait main");
+            const selectOptions = document.getElementById("options");
+            const colors = product.colors;
+            for (let color of colors) {
+                const option = document.createElement("option");
+                option.innerText = color;
+                option.setAttribute("value", color);
+                selectOptions.appendChild(option);
+            }
         }
     }
 }
 getProductObject();
 
 
-
-
-// affichage des données produit (breadcrumb, titre, image, description, prix)
-
-
-
-
-
-
-
-// gestion du clic sur bouton add to cart (données produit + quantité ---> plusieurs produits possibles)
+// gestion du clic sur bouton add to cart (données produit + quantité ---> localStorage - plusieurs produits possibles)
